cb18ad78654ab0a0c7ac38475f515b8b1f37f234
Jur Balledux <j.balledux@simgroep.nl>
2015-03-12 10:55:56 +0100

    Updated to latest spray-serializer and added test to be sure datetimeimmutable gets serialized

--- test/SerializerIntegrationTest.php
+++ test/SerializerIntegrationTest.php
@@ -2,9 +2,10 @@
 
 namespace Spray\SerializerBundle;
 
+use DateTime;
+use DateTimeImmutable;
 use Spray\BundleIntegration\IntegrationTestCase;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
-use DateTime;
 
 class SerializerIntegrationTest extends IntegrationTestCase
 {
@@ -21,9 +22,18 @@ class SerializerIntegrationTest extends IntegrationTestCase
         return $this->createContainer()->get('spray_serializer');
     }
     
-    public function testSerializeTagged()
+    public function testSerializeTaggedDateTime()
     {
         $date = DateTime::createFromFormat('Y-m-d H:i:s', '2015-01-01 12:00:00');
+        $this->assertSame(
+            '2015-01-01 12:00:00',
+            $this->createSerializer()->serialize($date)
+        );
+    }
+    
+    public function testSerializeTaggedDateTimeImmutable()
+    {
+        $date = DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2015-01-01 12:00:00');
         $this->assertSame(
             '2015-01-01 12:00:00',
             $this->createSerializer()->serialize($date)
